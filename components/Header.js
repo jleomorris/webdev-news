e5e54eb1630d13e582c750cd16d88d76c8024f88
James Morris <james-morris2010@hotmail.co.uk>
2021-08-20 17:38:36 +0100

    Remove example styled JSX in Header

--- components/Header.js
+++ components/Header.js
@@ -3,16 +3,12 @@ import headerStyles from "../styles/Header.module.css";
 const Header = () => {
   return (
     <div>
-      <h1 className="title">
-        <span>WebDev</span>News
+      <h1 className={headerStyles.title}>
+        <span>WebDev</span> News
       </h1>
-      <style>
-        {`
-          .title {
-            color: red;
-          }
-        `}
-      </style>
+      <p className={headerStyles.description}>
+        Keep up to date with the latest developer news
+      </p>
     </div>
   );
 };
